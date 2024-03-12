d2344377761d7edf8114859f4a03cba52f7a9665
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-12 18:06:19 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question no 39   City Names
+function city_country(city, country) {
+    console.log(`"${city}", "${country}"`);
+}
+city_country('karachi', 'pakistan');
+city_country('cairo', 'egypt');
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Question no 39   City Names
+
+function city_country(city:string, country:string){
+    console.log(`"${city}", "${country}"`)
+}
+
+city_country('karachi','pakistan')
+city_country('cairo','egypt')
